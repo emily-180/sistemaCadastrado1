92c0080c1293ffcedaae6890a0b4f492474e14bf
Emily Moreira Ferreira <49401027854@CAMPUS.MACHADO.MCH>
2022-11-03 10:11:26 -0300

    inicio

---
+++ SistemaCadastro/ConectaBanco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SistemaCadastro
+{
+    internal class ConectaBanco
+    {
+        MySqlConnection conexao = new MySqlConnection("server=localhost;user id=root;password=;database=banco_cadastro");
+        public String mensagem;
+
+        public DataTable listaGeneros()
+        {
+            MySqlCommand cmd = new MySqlCommand("lista_generos", conexao);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                conexao.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable tabela = new DataTable();
+                da.Fill(tabela);
+                return tabela;
+            }// fim try
+            catch (MySqlException e)
+            {
+                mensagem = "Erro:" + e.Message;
+                return null;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }// fim lista_generos
+
+    }
+}
