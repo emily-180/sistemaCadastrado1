ab3e3b3365006db57e6722308604c35feeb2f6d0
emily ferreira <emily.moreira.8@gmail.com>
2022-11-08 10:01:25 -0300

    adicionando bandas

--- SistemaCadastro/ConectaBanco.cs
+++ SistemaCadastro/ConectaBanco.cs
@@ -38,5 +38,31 @@ namespace SistemaCadastro
 
         }// fim lista_generos
 
+        public bool insereBanda(Banda b)
+        {
+            MySqlCommand cmd = new MySqlCommand("proc_insereBanda", conexao);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("nome", b.Nome);
+            cmd.Parameters.AddWithValue("genero", b.Genero);
+            cmd.Parameters.AddWithValue("integrantes", b.Integrantes);
+            cmd.Parameters.AddWithValue("ranking", b.Ranking);
+            try
+            {
+                conexao.Open();
+                cmd.ExecuteNonQuery(); // executa o comando
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                mensagem = "Erro:" + e.Message;
+                return false;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }// fim insereBanda
+
+
     }
 }
